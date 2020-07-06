ab169d732a9bba7712077acac79ac3f0735a3768
XXXX <879786154@qq.com>
2020-07-06 22:46:56 +0800

    Revert "init"
    This reverts commit 7b4693a65adc740c2ade7d542b54aa05f9f28dfe.

--- src/constants/CustomTheme.js
+++
@@ -1,5 +0,0 @@
-/* custom define */
-export const color_red = '#E93D34';
-export const color_primary = '#52c41a'; 
-export const color_white = '#fff';
-
--- src/models/app.js
+++
@@ -1,8 +0,0 @@
-export default {
-  namespace: 'app',
-  state: {
-    isLogin: false,
-  },
-  reducers: {
-  },
-}
